e946c349d0b4f47332bc339fcb386e43145ede9b
deruga <derekli.3628@gmail.com>
2019-01-31 02:10:20 -0500

    performed 20 epochs, restructured program by separating learn mode and tweet mode

--- bot.py
+++ bot.py
@@ -8,79 +8,123 @@ from keras.layers import Dropout
 from keras.layers import LSTM
 from keras.callbacks import ModelCheckpoint
 from keras.utils import np_utils
+from keras.models import load_model
 from credentials import *
 import tweepy
 from time import sleep
+import argparse
 
-# Access and authorize our Twitter credentials from credentials.py
-auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
-auth.set_access_token(access_token, access_token_secret)
-api = tweepy.API(auth)
+parser = argparse.ArgumentParser(description='Twitter bot')
+parser.add_argument('-l', help='learn mode', metavar='model_file')
+parser.add_argument('-t', help='tweet mode', action='store_true')
 
-# load ascii text and covert to lowercase
-filename = "wonderland.txt"
-raw_text = open(filename).read()
-raw_text = raw_text.lower()
-# create mapping of unique chars to integers, and a reverse mapping
-chars = sorted(list(set(raw_text)))
-char_to_int = dict((c, i) for i, c in enumerate(chars))
-int_to_char = dict((i, c) for i, c in enumerate(chars))
-# summarize the loaded data
-n_chars = len(raw_text)
-n_vocab = len(chars)
-print("Total Characters: ", n_chars)
-print("Total Vocab: ", n_vocab)
-# prepare the dataset of input to output pairs encoded as integers
-seq_length = 100
-dataX = []
-dataY = []
-for i in range(0, n_chars - seq_length, 1):
-    seq_in = raw_text[i:i + seq_length]
-    seq_out = raw_text[i + seq_length]
-    dataX.append([char_to_int[char] for char in seq_in])
-    dataY.append(char_to_int[seq_out])
-n_patterns = len(dataX)
-print("Total Patterns: ", n_patterns)
-# reshape X to be [samples, time steps, features]
-X = numpy.reshape(dataX, (n_patterns, seq_length, 1))
-# normalize
-X = X / float(n_vocab)
-# one hot encode the output variable
-y = np_utils.to_categorical(dataY)
-# define the LSTM model
-model = Sequential()
-model.add(LSTM(256, input_shape=(X.shape[1], X.shape[2])))
-model.add(Dropout(0.2))
-model.add(Dense(y.shape[1], activation='softmax'))
-model.compile(loss='categorical_crossentropy', optimizer='adam')
+opt_args = parser.parse_args()
 
-filepath="weights-improvement-{epoch:02d}-{loss:.4f}.hdf5"
-checkpoint = ModelCheckpoint(filepath, monitor='loss', verbose=1, save_best_only=True, mode='min')
-callbacks_list = [checkpoint]
-# fit the model
-model.fit(X, y, epochs=20, batch_size=128, callbacks=callbacks_list)
-# load the network weights
-filename = "weights-improvement-19-1.9435.hdf5"
-model.load_weights(filename)
+if opt_args.t:
+    
+    filename = "wonderland.txt"
+    raw_text = open(filename).read()
+    raw_text = raw_text.lower()
+    # create mapping of unique chars to integers, and a reverse mapping
+    chars = sorted(list(set(raw_text)))
+    char_to_int = dict((c, i) for i, c in enumerate(chars))
+    int_to_char = dict((i, c) for i, c in enumerate(chars))
+    # summarize the loaded data
+    n_chars = len(raw_text)
+    n_vocab = len(chars)
+    print("Total Characters: ", n_chars)
+    print("Total Vocab: ", n_vocab)
+    # prepare the dataset of input to output pairs encoded as integers
+    seq_length = 100
+    dataX = []
+    dataY = []
+    for i in range(0, n_chars - seq_length, 1):
+        seq_in = raw_text[i:i + seq_length]
+        seq_out = raw_text[i + seq_length]
+        dataX.append([char_to_int[char] for char in seq_in])
+        dataY.append(char_to_int[seq_out])
+    n_patterns = len(dataX)
+    print("Total Patterns: ", n_patterns)
+    # reshape X to be [samples, time steps, features]
+    X = numpy.reshape(dataX, (n_patterns, seq_length, 1))
+    # normalize
+    X = X / float(n_vocab)
+    # one hot encode the output variable
+    y = np_utils.to_categorical(dataY)
+    # define the LSTM model
+    model = Sequential()
+    model.add(LSTM(256, input_shape=(X.shape[1], X.shape[2])))
+    model.add(Dropout(0.2))
+    model.add(Dense(y.shape[1], activation='softmax'))
+    model.compile(loss='categorical_crossentropy', optimizer='adam')
 
+    weights_file = "weights-improvement-20-2.0531.hdf5"
 
+    model.load_weights(weights_file)
 
+    # pick a random seed
+    start = numpy.random.randint(0, len(dataX)-1)
+    pattern = dataX[start]
+    print("Seed:")
+    print("\"", ''.join([int_to_char[value] for value in pattern]), "\"")
+    # generate characters
+    # Access and authorize our Twitter credentials from credentials.py
+    # auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+    # auth.set_access_token(access_token, access_token_secret)
+    # api = tweepy.API(auth)
 
+    while True:
+        for i in range(1000):
+            x = numpy.reshape(pattern, (1, len(pattern), 1))
+            x = x / float(n_vocab)
+            prediction = model.predict(x, verbose=0)
+            index = numpy.argmax(prediction)
+            result = int_to_char[index]
+            seq_in = [int_to_char[value] for value in pattern]
+            sys.stdout.write(result)
+            pattern.append(index)
+            pattern = pattern[1:len(pattern)]
 
-# pick a random seed
-start = numpy.random.randint(0, len(dataX)-1)
-pattern = dataX[start]
-print("Seed:")
-print("\"", ''.join([int_to_char[value] for value in pattern]), "\"")
-# generate characters
-for i in range(1000):
-    x = numpy.reshape(pattern, (1, len(pattern), 1))
-    x = x / float(n_vocab)
-    prediction = model.predict(x, verbose=0)
-    index = numpy.argmax(prediction)
-    result = int_to_char[index]
-    seq_in = [int_to_char[value] for value in pattern]
-    sys.stdout.write(result)
-    pattern.append(index)
-    pattern = pattern[1:len(pattern)]
-print("\nDone.")
+if opt_args.l:
+    filename = "wonderland.txt"
+    raw_text = open(filename).read()
+    raw_text = raw_text.lower()
+    # create mapping of unique chars to integers, and a reverse mapping
+    chars = sorted(list(set(raw_text)))
+    char_to_int = dict((c, i) for i, c in enumerate(chars))
+    int_to_char = dict((i, c) for i, c in enumerate(chars))
+    # summarize the loaded data
+    n_chars = len(raw_text)
+    n_vocab = len(chars)
+    print("Total Characters: ", n_chars)
+    print("Total Vocab: ", n_vocab)
+    # prepare the dataset of input to output pairs encoded as integers
+    seq_length = 100
+    dataX = []
+    dataY = []
+    for i in range(0, n_chars - seq_length, 1):
+        seq_in = raw_text[i:i + seq_length]
+        seq_out = raw_text[i + seq_length]
+        dataX.append([char_to_int[char] for char in seq_in])
+        dataY.append(char_to_int[seq_out])
+    n_patterns = len(dataX)
+    print("Total Patterns: ", n_patterns)
+    # reshape X to be [samples, time steps, features]
+    X = numpy.reshape(dataX, (n_patterns, seq_length, 1))
+    # normalize
+    X = X / float(n_vocab)
+    # one hot encode the output variable
+    y = np_utils.to_categorical(dataY)
+    # define the LSTM model
+    model = load_model(model_file)
+    model.add(LSTM(256, input_shape=(X.shape[1], X.shape[2])))
+    model.add(Dropout(0.2))
+    model.add(Dense(y.shape[1], activation='softmax'))
+    model.compile(loss='categorical_crossentropy', optimizer='adam')
+
+    filepath="weights-improvement-{epoch:02d}-{loss:.4f}.hdf5"
+    checkpoint = ModelCheckpoint(filepath, monitor='loss', verbose=1, save_best_only=True, mode='min')
+    callbacks_list = [checkpoint]
+    # fit the model
+    model.fit(X, y, epochs=20, batch_size=128, callbacks=callbacks_list)
+
